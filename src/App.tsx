e40aaf70f174c9a976e3a5b267e76d2bba1c0a52
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-07-19 19:02:07 +0900

    multi camel versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import { Card, CardBody, CardTitle, Page, PageSection } from '@patternfly/react-core'
+import * as camel3 from '@hawtio/camel-model-v3'
+import * as camel4 from '@hawtio/camel-model-v4'
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Page,
+  PageSection,
+  TextContent,
+  TextList,
+  TextListItem,
+} from '@patternfly/react-core'
 import { FC } from 'react'
 
 export const App: FC = () => {
+  console.log('Camel model versions:', camel3.apacheCamelModelVersion, camel4.apacheCamelModelVersion)
   return (
     <Page>
       <PageSection>
-        <Card>
+        <Card isFullHeight>
           <CardTitle>Test App</CardTitle>
-          <CardBody>Hello!</CardBody>
+          <CardBody>
+            <TextContent>
+              <TextList component='dl'>
+                <TextListItem component='dt'>Camel v3</TextListItem>
+                <TextListItem component='dd'>{camel3.apacheCamelModelVersion}</TextListItem>
+                <TextListItem component='dt'>Camel v4</TextListItem>
+                <TextListItem component='dd'>{camel4.apacheCamelModelVersion}</TextListItem>
+              </TextList>
+            </TextContent>
+          </CardBody>
         </Card>
       </PageSection>
     </Page>
